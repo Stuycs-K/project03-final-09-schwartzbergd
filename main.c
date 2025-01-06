670f9787d30359fa5215f5370b3754955478d976
David Schwartzberg <dschwarzberg50@stuy.edu>
2025-01-06 13:34:01 -0600

    able to create files

--- main.c
+++ main.c
@@ -1,14 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <errno.h>
-
-char error_message[100];
+#include <fcntl.h>
+#include <unistd.h>
 
 int main(int argc, char *argv[]) {
 	if (argc != 2) {
 		printf("argc must be 2: you entered argc=%d\n", argc);
 		exit(1);
 	}
-	printf("your filename is: %s\n", argv[1]);
+	char *filename = argv[1];
+	printf("your filename is: %s\n", filename);
+
+	int file_descriptor = open(filename, O_RDWR | O_CREAT, 0644);
 	
+	printf("fd is %d\n", file_descriptor);
 }
