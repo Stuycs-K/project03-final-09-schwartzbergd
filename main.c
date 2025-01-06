a1318de164ba3f8ea198c2412144fa5c2891c322
David Schwartzberg <dschwarzberg50@stuy.edu>
2025-01-06 13:28:05 -0600

    simple parsing of argv

--- main.c
+++ main.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+
+char error_message[100];
 
 int main(int argc, char *argv[]) {
-	printf("args are:\n");
-	for (int i = 0; i < argc; i++) {
-		printf("%d: %s\n", i, argv[i]);
+	if (argc != 2) {
+		printf("argc must be 2: you entered argc=%d\n", argc);
+		exit(1);
 	}
-	
-	printf("hello from the main file\n");
+	printf("your filename is: %s\n", argv[1]);
 	
 }
