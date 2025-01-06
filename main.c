5436716b85e9ae97caafd0e6a655950ce982751b
David Schwartzberg <dschwarzberg50@stuy.edu>
2025-01-06 13:46:02 -0600

    able to read unbuffered char input from stdin (without waiting for enter)

--- main.c
+++ main.c
@@ -4,16 +4,31 @@
 #include <errno.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <termios.h>
 
 int main(int argc, char *argv[]) {
 	if (argc != 2) {
 		printf("argc must be 2: you entered argc=%d\n", argc);
 		exit(1);
 	}
+
+	struct termios old;
+        struct termios new;
+        tcgetattr(0, &old);
+        new = old;
+        new.c_lflag &= ~(ICANON | ECHO);
+        tcsetattr(0, TCSANOW, &new);
+
 	char *filename = argv[1];
 	printf("your filename is: %s\n", filename);
 
 	int file_descriptor = open(filename, O_RDWR | O_CREAT, 0644);
 	
 	printf("fd is %d\n", file_descriptor);
+	
+	printf("waiting for char input\n");
+	char c = getchar();
+	printf("char c is %c\n", c);
+
+	tcsetattr(0, TCSANOW, &old);
 }
